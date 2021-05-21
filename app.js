99bf362b37bb1b9cca353fb70e69116d5bb7608d
Praveen Yadav <yadavpraveen1023@gmail.com>
2021-05-21 17:00:51 +0530

    GymWebsiteLandingPage

---
+++ app.js
@@ -0,0 +1,7 @@
+    let ham = document.getElementById('ham');
+    let nav = document.getElementById('navi');
+
+    function bringmenu(){
+       nav.classList.toggle('open');
+        ham.classList.toggle('active');
+    }
